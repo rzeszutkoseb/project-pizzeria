26d7f6f2d031f2ac5d9e5c1dde29679b955cb825
Sebastian Rzeszutko <rzeszutkoseb@gmail.com>
2025-10-07 09:29:38 +0200

    add getElements, initOrderForm, processOrder

--- src/js/script.js
+++ src/js/script.js
@@ -60,7 +60,10 @@ const select = {
           thisProduct.data = data;
 
           thisProduct.renderInMenu();
+          thisProduct.getElements();
           thisProduct.initAccordion();
+          thisProduct.initOrderForm();
+          thisProduct.processOrder();
 
           console.log('new Product:', thisProduct);
       }
@@ -78,7 +81,18 @@ const select = {
 
       }
 
-       initAccordion() {
+      getElements(){
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+
+      }
+
+      initAccordion() {
       const thisProduct = this;
 
       /* find the clickable trigger (the element that should react to clicking) */
@@ -101,12 +115,74 @@ const select = {
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
       });
     }
+
+    initOrderForm(){
+      const thisProduct = this;
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder(){
+      const thisProduct = this;
+
+      // convert form to object structure, e.g. { sauce: ['tomato'], toppings: ['olives', 'salami'] }
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      // console.log('formData', formData);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings'
+        const param = thisProduct.data.params[paramId];
+        // console.log(paramId, param);
+
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives'
+          const option = param.options[optionId];
+          // console.log(optionId, option);
+
+          // check if option is selected in the form
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+
+          if(optionSelected) {
+            // if selected and NOT default -> add price
+            if(!option.default){
+              price += option.price;
+            }
+          } else {
+            // if NOT selected and IS default -> subtract price
+            if(option.default){
+              price -= option.price;
+            }
+          }
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+    }
   }
 
   const app = {
     initData: function () {
       const thisApp = this;
-
       thisApp.data = dataSource;
     },
 
